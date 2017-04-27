2a44485426902d8bf16730e20185599f5718576f
ArvilList <karriuss@gmail.com>
2017-04-27 18:06:56 +0300

    Starting users

--- feelings/settings.py
+++ feelings/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'thoughts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'asserts'),]
+
+#Authentication
+LOGIN_URL = 'users:login'
